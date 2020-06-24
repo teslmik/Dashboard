1855788ab3f6cabf2e0352203ec7928ba076f59f
Teslmik <teslmik@gmail.com>
2020-06-24 15:35:34 +0300

    First upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var dataset = [
+  {
+    value: 35,
+    color: '#567DF4'
+  }, {
+    value: 35,
+    color: '#22215B'
+  }, {
+    value: 15,
+    color: '#FFC700'
+  }, {
+    value: 15,
+    color: '#4CE364'
+  }
+];
+
+var maxValue = 25;
+var container = $('.diagram-circle');
+
+var addSector = function (data, startAngle, collapse) {
+  var sectorDeg = 3.6 * data.value;
+  var skewDeg = 90 + sectorDeg;
+  var rotateDeg = startAngle;
+  if (collapse) {
+    skewDeg++;
+  }
+
+  var sector = $('<div>', {
+    'class': 'sector'
+  }).css({
+    'background': data.color,
+    'transform': 'rotate(' + rotateDeg + 'deg) skewY(' + skewDeg + 'deg)'
+  });
+  container.append(sector);
+
+  return startAngle + sectorDeg;
+};
+
+dataset.reduce(function (prev, curr) {
+  return (function addPart(data, angle) {
+    if (data.value <= maxValue) {
+      return addSector(data, angle, false);
+    }
+
+    return addPart({
+      value: data.value - maxValue,
+      color: data.color
+    }, addSector({
+      value: maxValue,
+      color: data.color,
+    }, angle, true));
+  })(curr, prev);
+}, 0);
